bea6db1f113481936c2c0ac09e9ab9f630fd983b
Tanguy <tanguy.legoff@univ-brest.fr>
2023-02-14 16:17:49 +0100

    Refactoring a nouveau

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "getPoints.h"
 
 // genPoints <nbPoints> [<seed>]
@@ -36,7 +35,7 @@ visibilite = [
 /*
  * Cette fonction affiche la liste des multiples de n_points du arr->matriceleau matrice
  */
-void print_list_multiples(modular_array *arr) {
+__attribute__((unused)) void print_list_multiples(modular_array *arr) {
     for (int k = 0; k < arr->n_multiples; ++k) {
         printf("[");
         for (int l = 0; l < 2; ++l) {
@@ -76,6 +75,7 @@ modular_array *construct_modular_array() {
     for (int k = 0; k < arr->taille; ++k) {
         arr->matrice[k] = (int *) malloc(sizeof(int) * 2);
     }
+    return arr;
 }
 
 /*
@@ -100,6 +100,23 @@ void check_multiple(int k, int l, int n_points, modular_array *arr) {
     }
 }
 
+void print_point(int i, int j, int k, int l) {
+    if (k == i && l == j) {
+        printf("< %d, %d>\n", k, l);
+    } else
+        printf("< %d, %d>,\n", k, l);
+}
+
+void print_points(int i, int j) {
+    printf("points = {\n");
+    for (int k = 1; k < i + 1; ++k) {
+        for (int l = 1; l < j + 1; ++l) {
+            print_point(i, j, k, l);
+        }
+    }
+    printf("};\n");
+}
+
 /*
  * Cette fonction cherche les multiples de n_points les plus équilibrés pour faire la matrice de points
  */
@@ -116,6 +133,7 @@ void find_multiples(int n_points, int *i, int *j) {
     }
 
     if (arr->n_multiples == 0) {
+
         fprintf(stderr, "Erreur, pas de multiples de n trouvés\n");
         exit(1);
     }
@@ -130,9 +148,38 @@ void find_multiples(int n_points, int *i, int *j) {
     free_modular_array(arr);
 }
 
-int main(int argc, char *argv[]) {
-    struct modular_array test;
+/*
+ * Donne un chiffre 0 ou 1, en fonction d'un pourcentage donné en paramètre.
+ * Si le chiffre est de 80, la fonction donnerait un 1 80 fois et un 0 20 fois du temps.
+ * Donc si le chiffre est de 20, le 1 serait retourné 20 fois et 0 80 fois.
+ */
+int get_visibility_information(int pourcentage) {
+    if (pourcentage < 0 || pourcentage > 99) {
+        fprintf(stderr, "Erreur de paramètre. Pourcentage n'est pas dans les bornes (0, 99) requis. "
+                        "Le paramètre est : %d", pourcentage);
+        return -1;
+    }
+    // Donne les 1 ou 0 de la table de visibilité en fonction d'une probabilité
+    // (ici 66% de chance que la fonction donne un 1)
+    int res = rand() % 100;
+    if (res > pourcentage) res = 1;
+    else res = 0;
+    return res;
+}
 
+void print_visibility_array(int n_points, int pourcentage) {
+    printf("\nvisibilite = [\n");
+    for (int k = 0; k < n_points; ++k) {
+        printf("[");
+        for (int l = 0; l < n_points; ++l) {
+            printf(" %d ", get_visibility_information(pourcentage));
+        }
+        printf("]\n");
+    }
+    printf("];\n");
+}
+
+int main(int argc, char *argv[]) {
     int i, j;
     if (argc < 2) goto usage;
 
@@ -143,33 +190,17 @@ int main(int argc, char *argv[]) {
     if (n_points <= 8) {
         n_points = 8;
     }
-//    n_points = 13 + 1;
+
     printf("n_points = %d\n", n_points);
 
     find_multiples(n_points, &i, &j);
 
-    printf("points = {\n");
-    for (int k = 1; k < i + 1; ++k) {
-        for (int l = 1; l < j + 1; ++l) {
-            if (k == i && l == j) {
-                printf("< %d, %d>\n", k, l);
-            } else
-                printf("< %d, %d>,\n", k, l);
-        }
-    }
-    printf("};\n");
+    print_points(i, j);
 
-    printf("\nvisibilite = [\n");
-    for (int k = 0; k < n_points; ++k) {
-        printf("[");
-        for (int l = 0; l < n_points; ++l) {
-            printf(" %d ", rand() % 2);
-        }
-        printf("]\n");
-    }
-    printf("];\n");
+    print_visibility_array(n_points, 66);
 
     return 0;
+
     usage:
     fprintf(stderr, "usage: %s <nbPoints> [<seed>]\n", argv[0]);
     return 1;
